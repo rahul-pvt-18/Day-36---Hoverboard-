3c7d42f0d9860f21b643a53bbd407a73d347828b
Rahul Love <100333864+rahul-pvt-18@users.noreply.github.com>
2023-07-22 23:07:39 +0530

    first commit
    Uploading HTML, CSS and JavaScript files

---
+++ Day 36 - Hoverboard/script.js
@@ -0,0 +1,28 @@
+const container = document.getElementById('container');
+const colors = ['#9400D3','#4B0082','#0000FF','#00FF00','#FFFF00','#FF7F00','#FF0000',];
+const SQUARES = 500;
+
+for(let i = 0 ;i < SQUARES; i++){
+    const square  = document.createElement('div');
+    square.classList.add('square');
+
+    square.addEventListener('mouseover',() => setColor(square));
+    square.addEventListener('mouseout',() => removeColor(square));
+
+    container.appendChild(square);
+}
+
+function setColor(element){
+    const color = getRandomColor();
+    element.style.background = color;
+    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
+}
+
+function removeColor(element){
+    element.style.background = `#1d1d1d`;
+    element.style.boxShadow = `0 0 2px #000`;
+}
+
+function getRandomColor(){
+    return colors[Math.floor(Math.random() * colors.length)];
+}
